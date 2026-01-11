ddd153738e55a09cbc4348a0200031d547186ccc
Elie Habib <elie.habib@gmail.com>
2026-01-11 19:32:41 +0400

    Add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './styles/main.css';
+import { inject } from '@vercel/analytics';
 import { App } from './App';
+
+// Initialize Vercel Analytics
+inject();
 
 const app = new App('app');
 app.init().catch(console.error);
