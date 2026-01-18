2a9ba58fa4ea0d096d968f104b6f555505a51ab7
Elie Habib <elie.habib@gmail.com>
2026-01-18 12:06:11 +0400

    Update header version to 1.4.0

--- src/App.ts
+++ src/App.ts
@@ -524,7 +524,7 @@ export class App {
     this.container.innerHTML = `
       <div class="header">
         <div class="header-left">
-          <span class="logo">WORLD MONITOR</span><span class="version">v1.3.9</span>
+          <span class="logo">WORLD MONITOR</span><span class="version">v1.4.0</span>
           <a href="https://x.com/eliehabib" target="_blank" rel="noopener" class="credit-link">
             <svg class="x-logo" width="14" height="14" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
             <span class="credit-text">@eliehabib</span>
